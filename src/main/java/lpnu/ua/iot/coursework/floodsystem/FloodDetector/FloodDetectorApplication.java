d499c0a7807078f46ead532a5149cc8f713c3f40
Danvace <vipdan055@gmail.com>
2023-05-03 21:57:10 +0300

    add springboot to project

---
+++ src/main/java/lpnu/ua/iot/coursework/floodsystem/FloodDetector/FloodDetectorApplication.java
@@ -0,0 +1,13 @@
+package lpnu.ua.iot.coursework.floodsystem.FloodDetector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FloodDetectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FloodDetectorApplication.class, args);
+	}
+
+}
